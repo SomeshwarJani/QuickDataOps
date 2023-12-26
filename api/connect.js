45de4098ca9aab348c1a18610cc1a66996a06c5f
Milan Sonani <sjani9192@gmail.com>
2023-12-27 00:51:51 +0530

    new db connection

--- api/connect.js
+++ api/connect.js
@@ -1,10 +1,13 @@
 import mysql from "mysql";
 
 const DATABASE_CONNECT = {
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "user_data",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  waitForConnection: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 };
 
 export const connection = mysql.createConnection(DATABASE_CONNECT);
