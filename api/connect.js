0a415a81af07534585ecf2870d184e19a58ddb51
Milan Sonani <sjani9192@gmail.com>
2023-12-27 15:48:50 +0530

    db changes

--- api/connect.js
+++ api/connect.js
@@ -3,14 +3,14 @@ import mysql from "mysql";
 const DATABASE_CONNECT = {
   host: "bxka49jlb5vqk7bsekub-mysql.services.clever-cloud.com",
   user: "u05zepe8u9omexoa",
-  password: "u05zepe8u9omexoa",
+  password: "cgVuxqDfH8vbmeB20HgD",
   database: "bxka49jlb5vqk7bsekub",
   waitForConnection: true,
   connectionLimit: 10,
   queueLimit: 0,
 };
 
-const connection = mysql.createPool(DATABASE_CONNECT);
+export const connection = mysql.createPool(DATABASE_CONNECT);
 
 connection.getConnection((err, connection) => {
   if (err) {
