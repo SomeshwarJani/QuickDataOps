663e7efca3abc1ca8ccd8690ec1bb9f44a9cba88
Milan Sonani <sjani9192@gmail.com>
2023-12-27 13:06:59 +0530

    necessary changes

--- api/index.js
+++ api/index.js
@@ -12,7 +12,10 @@ import deleteData from "./routes/delete.js";
 const app = Express();
 //middlewares
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://user-data-task-clients.vercel.app"
+  );
   res.setHeader("Access-Control-Allow-Methods", "POST");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -32,7 +35,7 @@ app.use("/api", updateData);
 app.use("/api", deleteData);
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://user-data-task-clients.vercel.app",
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
   })
