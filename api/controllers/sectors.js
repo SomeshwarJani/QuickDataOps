dcd0329add8f2b8a9bc63d4a5b4eb231c3003c50
Milan Sonani <sjani9192@gmail.com>
2023-12-27 16:06:43 +0530

    connection changes

--- api/controllers/sectors.js
+++ api/controllers/sectors.js
@@ -5,12 +5,12 @@ export const getSectors = (req, res) => {
   const q = `SELECT id,sector_name as label, value, parentId FROM user_data.sector_detail;`;
   connection.query(q, (error, data) => {
     if (error) return res.status(500).json(error);
-    // res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Origin", "*");
     const token = jwt.sign({ id: data[0].id }, "secretKey");
-    res.set(
-      "Access-Control-Allow-Origin",
-      "https://user-data-task-clients.vercel.app"
-    );
+    // res.set(
+    //   "Access-Control-Allow-Origin",
+    //   "https://user-data-task-clients.vercel.app"
+    // );
     res
       .cookie("accessToken", token, {
         httpOnly: true,
