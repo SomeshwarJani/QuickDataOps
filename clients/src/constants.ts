350788258154a0df67b4b633967fc1c38dc04485
Milan Sonani <sjani9192@gmail.com>
2023-12-27 18:41:52 +0530

    clients build

--- clients/src/constants.ts
+++ clients/src/constants.ts
@@ -12,9 +12,9 @@ enum baseURL {
 }
 export enum APIS {
   FETCHSECTORS = "https://user-data-task-server.vercel.app/api/sectors",
-  FETCH_SELECTED_ROW_DATA = "https://user-data-task-server.vercel.app/api/edit",
+  FETCH_SELECTED_ROW_DATA = "https://user-data-task-server.vercel.app/api/edit/",
   SUBMITFORM = `https://user-data-task-server.vercel.app/api/submit`,
   UPDATEDATA = "https://user-data-task-server.vercel.app/api/update",
-  DELETEDATA = "https://user-data-task-server.vercel.app/api/delete",
+  DELETEDATA = "https://user-data-task-server.vercel.app/api/delete/",
   FETCHUSERDATA = "https://user-data-task-server.vercel.app/api/userdata",
 }
