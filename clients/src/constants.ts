c8de6a712e1742344463dd7a0696217588dd5bdc
Milan Sonani <sjani9192@gmail.com>
2023-12-27 19:24:24 +0530

    new build deployed

--- clients/src/constants.ts
+++ clients/src/constants.ts
@@ -7,9 +7,6 @@ export enum STATICSTRINGS {
   IDSTRING = "/?id=",
   ID = "id",
 }
-enum baseURL {
-  url = "https://user-data-task-clients.vercel.app/",
-}
 export enum APIS {
   FETCHSECTORS = "https://user-data-task-server.vercel.app/api/sectors",
   FETCH_SELECTED_ROW_DATA = "https://user-data-task-server.vercel.app/api/edit/",
